b8e5f23544b0f0a61297bffc91154fc21150924c
Shashank singh bisht <Shashank.singh2016.ss@gmail.com>
2021-05-01 18:52:46 +0530

    Last Commit after upload updates

---
+++ library/build/generated/source/buildConfig/androidTest/debug/jp/co/cyberagent/android/gpuimage/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package jp.co.cyberagent.android.gpuimage.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "jp.co.cyberagent.android.gpuimage.test";
+  public static final String BUILD_TYPE = "debug";
+}
